e3bdeb72093469c98c1599ded9ab0bdc322bee3b
tohu.masao <konoemario@gmail.com>
2019-01-13 15:24:54 +0900

    初期設定

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PostList from './posts/PostList';
+
+const App = () => (
+  <div>
+    <h1>PostList</h1>
+    <PostList />
+  </div>
+);
+
+export default App;
---
+++ src/components/posts/PostList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PostList extends React.Component {
+  render() {
+    return (
+      <div className="post-list">
+        <div className="post-item">
+          item
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PostList;
