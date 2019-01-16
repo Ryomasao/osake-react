ed6820f9ee35c6c6a5622d9211aaed6c2fe3105c
ryoji.yamauchi <ryoji_yamauchi@waku-2.com>
2019-01-16 19:07:05 +0900

    最低限のサンプルを追加

--- src/components/posts/PostForm.js
+++ src/components/posts/PostForm.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import { Formik, Field, Form, FiledArray, ErrorMessage } from 'formik';
 
-const initialValues = {
-  friends: [
-    {
-      name: '',
-      email: ''
-    }
-  ]
-};
+class PostForm extends React.Component {
+  state = {
+    image: null,
+    memo: '',
+    date: '',
+    favos: 0,
+  }
 
-const PostForm = () => (
-  <div>
-    <h1>UseFormik</h1>
-    <Formik
-      initialValues={initialValues}
-    >
-      <div>Foo</div>
-    </Formik>
-  </div>
-);
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>PlainPostForm</h1>
+        <div>
+          <label>しゃしん</label>
+          <input name="image" type="file"/>
+        </div>
+        <div>
+          <label>メモ</label>
+          <input 
+            name="memo" 
+            type="text"
+            value={this.state.memo}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <div>
+          <label>飲んだ日</label>
+          <input name="date" type="text"/>
+        </div>
+        <div>
+          <p>いいね： 0</p>
+          <button type="button">+</button>
+          <button type="button">-</button>
+        </div>
+        <div>
+          {JSON.stringify(this.state, null, 2)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default PostForm;
