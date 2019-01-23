7c45383b5ca0e4075d317aa07d7504a5eb976e90
ryoji.yamauchi <ryoji_yamauchi@waku-2.com>
2019-01-23 19:36:16 +0900

    styled-componentとblumaの併用いけそうな気がする

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import Header from './Header';
 import PostList from './posts/PostList';
 import PostShow from './posts/PostShow';
@@ -7,17 +8,21 @@ import PostCreate from './posts/PostCreate';
 import PostEdit from './posts/PostEdit';
 
 const App = () => (
-  <div className="container">
-    <BrowserRouter>
-      <div>
-        <Header></Header>
-        <Route path="/" exact component={PostList} />
-        <Route path="/posts/new" exact component={PostCreate} />
-        <Route path="/posts/show/:id" exact component={PostShow} />
-        <Route path="/posts/edit/:id" exact component={PostEdit} />
-      </div>
-    </BrowserRouter>
-  </div>
+  <BrowserRouter>
+    <Wrapper className="container">
+      <Header></Header>
+      <Route path="/" exact component={PostList} />
+      <Route path="/posts/new" exact component={PostCreate} />
+      <Route path="/posts/show/:id" exact component={PostShow} />
+      <Route path="/posts/edit/:id" exact component={PostEdit} />
+    </Wrapper>
+  </BrowserRouter>
 );
+
+const Wrapper = styled.div`
+  font-family: 'Sawarabi Gothic', sans-serif;
+`;
+
+
 
 export default App;
