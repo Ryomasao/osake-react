18d34bce85b71a77db7f72f4cd972c5f43c3e67e
tohu.masao <konoemario@gmail.com>
2019-01-27 20:12:14 +0900

    日付をYMDにする

--- src/components/molecules/PostItem.js
+++ src/components/molecules/PostItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const PostItem = ({ to, post }) => {
   return (
     <div>
       <Link to={to} >
-        <span>{post.date}</span>
+        <span>{post.date.slice(0,10)}</span>
         <img src={post.imagePath} alt="osake"/>
       </Link>
     </div>
