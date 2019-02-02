01d6d08f134b9d306a761e9c5b77753de631e2f0
tohu.masao <konoemario@gmail.com>
2019-02-02 15:16:32 +0900

    SVGではなく結局ただの画像を追加

---
+++ src/components/atoms/Icon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import fabo from '../../static/img/fabo.png';
+
+const Icon = () => {
+  return (
+    <div>
+      <img src={fabo} alt="fabo"/>
+    </div>
+  );
+};
+
+export default Icon;
